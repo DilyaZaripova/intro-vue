16c0649eb2ce25d31a7d1a9523122e47285d8a68
DilyaZaripova <fooont@bk.ru>
2021-04-26 12:55:49 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,106 @@
+https://codepen.io/VueMastery/pen/qxwZBQ//Add a description to the data object with the value "A pair of warm, fuzzy socks". Then display the description using an expression in an p element, underneath the h1.
+Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: `
+    <div class="product">
+  
+    <div class="product-image">
+      <img v-bind:src="image" />
+    </div>
+
+    <div class="product-info">
+      <h1>{{ title }}</h1>
+      <p v-if="inStock">In Stock</p>
+      
+      <p v-else style='text-decoration:line-through'>Out of Stock</p>
+      <p>Shipping: {{shipping}}</p>
+      
+      <ul>
+        <li v-for='detail in details'>{{ detail }}</li>
+      </ul>
+      <div v-for='(variant,index) in variants' 
+           :key='variant.variantId'
+           class='color-box'
+           :style='{backgroundColor: variant.variantColor}'
+           @mouseover='updateProduct(index)'>
+      </div>
+      
+      <button v-on:click='addToCart' :disabled='!inStock'
+              :class='{disabledButton: !inStock}'>Add to cart</button>
+      <button v-on:click='deleteFromCart'
+              :class='deleteCart'>Delete from cart</button>
+      
+    </div>
+  </div>`,
+  data() {
+    return {
+    brand: 'Vue Mastery',
+    product: 'Socks',
+    selectedVariant:0,
+    onSale:true,
+    details:['80% cotton','20% polyester','Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        variantQuantity:10
+      },
+      {
+        variantId: 2235,
+        variantColor:'blue',
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+        variantQuantity:0
+      }
+    ],
+    }
+  },
+    methods: {
+      addToCart(){
+        this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
+      },
+      deleteFromCart(){
+        this.$emit('delete-from-cart', this.variants[this.selectedVariant].variantId)
+      },
+      updateProduct(index) {
+        this.selectedVariant=index
+      }
+    },
+  computed: {
+    title(){
+      return this.brand + ' ' + this.product
+    },
+    image(){
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock(){
+  return this.variants[this.selectedVariant].variantQuantity
+},
+    shipping(){
+      if(this.premium){
+        return "Free"
+      } return 2.99
+    }
+  }
+})
+var app = new Vue({
+  el: '#app',
+  data: {
+      premium:false,
+      cart: []
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id)
+    },
+    deleteCart(id) {
+      this.cart.pop(id)
+    }
+  }
+})
+
